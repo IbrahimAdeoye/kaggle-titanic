ebf33c4b3c688134623e0d1f6b928a029a8ea4c7
IbrahimAdeoye <69392764+IbrahimAdeoye@users.noreply.github.com>
2023-08-25 16:21:20 +0100

    Update kaggle titanic.py

--- kaggle titanic.py
+++ kaggle titanic.py
@@ -1,22 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[20]:
-
-
 import numpy as np 
 import pandas as pd
-
-
-# In[29]:
-
 
 titanic_train = pd.read_csv('/Users/adeoyedipo/Downloads/titanic-4/train.csv')
 titanic_test = pd.read_csv('/Users/adeoyedipo/Downloads/titanic-4/test.csv')
 gender_sub =pd.read_csv('/Users/adeoyedipo/Downloads/titanic-4/gender_submission.csv')
-
-
-# In[22]:
 
 
 titanic_target = titanic_train["Survived"].copy()
@@ -24,9 +11,6 @@ titanic_train = titanic_train.drop(["Survived",'Name','Cabin','Ticket'], axis=1)
 titanic_train.set_index('PassengerId',inplace=True) #'PassengerId'
 titanic_test = titanic_test.drop(['Name','Cabin','Ticket'], axis=1)
 titanic_test.set_index('PassengerId',inplace=True)
-
-
-# In[23]:
 
 
 from sklearn.pipeline import make_pipeline
@@ -48,7 +32,6 @@ preprocessing = ColumnTransformer([
     ('cat',hot_encode,['Sex','Embarked'])],remainder='passthrough')
 
 
-# In[24]:
 
 
 from sklearn.ensemble import RandomForestClassifier
@@ -58,75 +41,12 @@ full_pipeline = make_pipeline(
 full_pipeline.fit(titanic_train,titanic_target)
 
 
-# In[25]:
 
 
 full_pipeline.predict(titanic_test)
 
 
-# In[ ]:
-
-
-
-
-
-# In[32]:
-
 
 # evaluate on the test set
 full_pipeline.score(titanic_test,gender_sub['Survived'])
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[60]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
